3c46d2b22959dcb3f257acc661f1df96d84763ae
localadmin <you@example.com>
2018-08-29 15:51:13 -0500

    dsgjds

--- YelpGoogle.sh
+++ YelpGoogle.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-yelptext=wget -q0- www.yelp.com | grep yelp | wc -l >> filename
+#yelptext=wget -q0- www.yelp.com | grep yelp | wc -l >> filename
+yelptext=wget -q www.yelp.com | grep yelp | wc -l >> filename
 sudo sed -n '50,$p' filename
 sed 's/yelp/google/p' filename > filename2
