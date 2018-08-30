574d60ffb8b8ce0b7c7c47e3cbb6fffd248f28a0
localadmin <you@example.com>
2018-08-30 13:27:00 -0500

    ggg

--- num2b.sh
+++ num2b.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo -n "Enter number 1 < x < 10: "
-read num1
-echo -n "Enter 2nd number 1 < x < 10:"
-read num2
 
 if [[ $num1 -lt 10 ]] || [[ "$num2" -lt 10 ]]; then
 		#echo "$num1 + $num2=$(($num1 + $num2))"
