098ad8cfe98c82de73a067ad95fae278a4ced5ba
localadmin <you@example.com>
2018-08-30 13:37:47 -0500

    a

--- num2b.sh
+++ num2b.sh
@@ -5,7 +5,7 @@ echo -n "Enter 2nd number 1 < x < 10:"
 read num2
 if [[ $num1 -lt 10 ]] || [[ "$num2" -lt 10 ]]; then
 		#echo "$num1 + $num2=$(($num1 + $num2))"
-        for i in $(seq 20)
+        for i in $(seq 10)
 	do
 		echo "Number=" $i
         done
