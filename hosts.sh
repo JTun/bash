6fc04fbe7ed0a039852a19e58f1f4413251cdef0
localadmin <you@example.com>
2018-08-30 14:29:49 -0500

    ssss

--- hosts.sh
+++ hosts.sh
@@ -1,2 +1,2 @@
-ssh -i "fullstack.pem" ec2-user@ec2-18-233-97-155.compute-1.amazonaws.com
-ssh -i "fullstack.pem" ec2-user@ec2-54-236-44-74.compute-1.amazonaws.com
+ec2-user@ec2-18-233-97-155.compute-1.amazonaws.com
+ec2-user@ec2-54-236-44-74.compute-1.amazonaws.com
