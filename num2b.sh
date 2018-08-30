84163fc5d91728c328afc287dc2b667a4bbecf05
localadmin <you@example.com>
2018-08-30 13:25:19 -0500

    aaa

---
+++ num2b.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -n "Enter number 1 < x < 10: "
+read num1
+echo -n "Enter 2nd number 1 < x < 10:"
+read num2
+
+if [[ $num1 -lt 10 ]] || [[ "$num2" -lt 10 ]]; then
+		#echo "$num1 + $num2=$(($num1 + $num2))"
+        for i in $(seq 10)
+	do
+		echo "Number=" $i
+        done
+	else
+    echo "$(( $num1 * $num2 ))"
+fi
